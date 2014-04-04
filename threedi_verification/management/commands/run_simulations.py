2bd107ea94d212f672bf640c4aea26c019447589
Reinout van Rees <reinout@vanrees.org>
2014-04-04 10:26:23 +0200

    Using django's call_command() instead of os.system()

--- threedi_verification/management/commands/run_simulations.py
+++ threedi_verification/management/commands/run_simulations.py
@@ -2,8 +2,9 @@ import logging
 import os
 import datetime
 
-from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
 
 from threedi_verification.models import TestCase
 LIBRARY_LOCATION = '/opt/3di/bin/subgridf90'
@@ -19,5 +20,4 @@ class Command(BaseCommand):
         testdir = settings.TESTCASES_ROOT
         for test_case in TestCase.objects.all():
             full_path = os.path.join(testdir, test_case.filename)
-            cmd = "bin/django run_simulation %s" % full_path
-            os.system(cmd)
+            call_command('run_simulation', full_path)
